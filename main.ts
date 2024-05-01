30aa2928bf3e8c136e20c062f00b0027e79d2a92
sobia-asif <sobiaarf1728@gmail.com>
2024-05-01 13:24:48 -0700

    push my todo list to github

---
+++ main.ts
@@ -0,0 +1,38 @@
+#! /usr/bin/env node
+import  inquirer from "inquirer";
+import chalk from "chalk";
+
+
+let todoList = []; 
+let conditions = true;
+
+console.log(chalk.blue("\n \t Todo-List\n"));
+
+while(conditions){
+   let addTask =await inquirer.prompt([
+    {
+        name: "task",
+        type: "input",
+        message: "Enter your New Task:"
+    } 
+]);
+todoList.push(addTask.task);
+console.log(`${addTask.task} Task add in Todo-List successfully`);
+
+let addMoreTask = await inquirer.prompt([
+    {
+        name: "addmore",
+        type : "confirm",
+        message: "Do you want to add more Task?",
+        default: "false"
+        
+    }
+
+]);
+
+conditions= addMoreTask.addmore
+
+}
+console.log("your updated todo-list:\n", todoList);
+
+
